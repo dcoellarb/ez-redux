97cfef141379ce437967d805e77d037f13b0187e
dcoellar <dcoellar@genexususa.com>
2016-09-07 11:09:55 -0500

    ready for publish

--- index.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-import getActions from './actions';
-import reducer from './reducer';
-import middleware from './middleware'
-
-export const api = api;
-export const getActions = getActions;
-export const reducer = reducer;
-export const middleware = middleware;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import api from './api';
+import getActions from './actions';
+import reducer from './reducer';
+import middleware from './middleware';
+
+export default { api, getActions, reducer, middleware };
