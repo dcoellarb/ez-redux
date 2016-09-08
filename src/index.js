8a1951fdcd9df2d205e1a8cee27b3a22013e2dea
dcoellar <dcoellar@genexususa.com>
2016-09-08 16:03:00 -0500

    fix selectors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import api from './api';
 import getActions from './actions';
 import reducer from './reducer';
+import * as selectors from './reducer';
 import middleware from './middleware';
 
-export default { api, getActions, reducer, middleware };
+export default { api, getActions, reducer, selectors, middleware };
