69f51e4456d6b77122c30a481d7d6e6cbce69ee3
dcoellar <dcoellar@genexususa.com>
2016-09-09 16:32:51 -0500

    fix initializer

--- src/helpers/initializer.js
+++ src/helpers/initializer.js
@@ -1,14 +1,24 @@
-export const findAndInitializeEntityConfig = (config, entity) => {
-  const foundConfig = config.find(e => e.name === entity);        
-  if (!foundConfig){
-    return foundConfig;    
-  }
-  return initializeEntityConfig(foundConfig)
-}
-
-const ignoredFields = ["object","errors","warnings"];
+const ignoredFields = ['object', 'errors', 'warnings'];
+const defaultEntityConfig = {
+  name: '',
+  mapArraysToFields: [],
+  mapPointersToFields: [],
+  mapRealtionsToFields: [],
+  nonStoredFields: [],
+  validate: () => ({
+    errors: [],
+    warnings: []
+  })
+};
 export const initializeEntityConfig = (foundConfig) => {
   const entityConfig = Object.assign(defaultEntityConfig, foundConfig);
   entityConfig.nonStoredFields = [...entityConfig.nonStoredFields, ...ignoredFields];
   return entityConfig;
-}
+};
+export const findAndInitializeEntityConfig = (config, entity) => {
+  const foundConfig = config.find(e => e.name === entity);
+  if (!foundConfig) {
+    return foundConfig;
+  }
+  return initializeEntityConfig(foundConfig);
+};
