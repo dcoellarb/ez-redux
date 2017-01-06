ea1a16f103f29dc4ac62de4f65b5883041addd85
dcoellar <dcoellar@genexususa.com>
2017-01-06 14:44:46 -0500

    fixes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import reducer from './reducer';
 import * as selectors from './reducer';
 import middlewareEnvChooser from './middleware';
 import * as serializer from './helpers/serializer';
+import * as initializer from './helpers/initializer';
 
 export default (parse) => {
   const api = apiEnvChooser(parse);
   const middleware = middlewareEnvChooser(parse);
-  return { api, getActions, reducer, selectors, middleware, serializer };
+  return { api, getActions, reducer, selectors, middleware, serializer, initializer };
 };
